76066cec93016d1d60e47908a66366df068542fa
Myroslav Mishchuk <myroslav.m437498@gmail.com>
2021-05-30 19:14:01 +0300

    empty command bug fixed

--- main.cpp
+++ main.cpp
@@ -7,14 +7,22 @@ int main(int argc, char* argv[]) {
     std::string expr;
 
     // concat all arguments:
+    if(argc < 2) {
+        std::cout << "No expression passed" << std::endl;
+        return 1;
+    }
     for(int i = 1; i < argc; ++i) {
         expr.append(argv[i]);
     }
     calc.setExpression(expr);
 
     // run calculator:
-    auto fut = std::async(std::launch::async, &Calculator::calculate, &calc);
-    std::cout << fut.get() << std::endl;
-
+    try {
+        auto fut = std::async(std::launch::async, &Calculator::calculate, &calc);
+        std::cout << fut.get() << std::endl;
+    }
+    catch(...) {
+        std::cout << "Error occured" << std::endl;
+    }
     return 0;
 }
